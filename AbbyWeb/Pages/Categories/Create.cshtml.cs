900c272f918178cc7426e49e13a8e6b6c2e5ceb0
SOunit <workbookwork7@gmail.com>
2022-08-29 11:48:04 -0700

    add validation

--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -23,6 +23,10 @@ namespace AbbyWeb.Pages.Categories
 
         public async Task<IActionResult> OnPost()
         {
+            if (Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Category.Name", "The Display Order cannot exactly match the Name.");
+            }
             if (ModelState.IsValid)
             {
                 await _db.Category.AddAsync(Category);
